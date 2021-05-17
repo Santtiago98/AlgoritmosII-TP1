37cfe59fb929eb262344768599fc29a7f10fec6c
lbuyi97 <lboudjikanian@fi.uba.ar>
2021-05-16 22:59:03 -0300

    prueba de commit desde consola

--- bignum.h
+++ bignum.h
@@ -2,16 +2,30 @@
 
 class bignum
 {
-private:
-unsigned short *digits;
-// ...INCOMPLETO --> AGREGAR LO QUE SEA NECESARIO 
+    private:
+        unsigned short *digits;
+        unsigned short precision; // prec=4 --> XXXX 
+		unsigned short efective_size
+        bool negative;      
+    // ...
 
-public:
-// ...INCOMPLETO --> AGREGAR LO QUE SEA NECESARIO 
+    public:
+    // ...
 
-friend bignum operator+(const bignum&, const bignum&);
-friend bignum operator-(const bignum&, const bignum&);
-friend bignum operator*(const bignum&, const bignum&);
-friend std::ostream& operator<<(std::ostream&, const bignum&);
-friend std::istream& operator>>(std::istream&, bignum&);
-};
+		~bignum();   //adentro liberar espacio
+
+		bignum(); //prec x defecto - Inicializado en 0
+		bignum(const unsigned short precision);     //util para resultado metodos
+		bignum(const string &, const unsigned short precision);   //"12315", p>=size(str+sign)
+		bignum(const bignum &);  //constr copia
+
+		friend bignum operator+(const bignum&, const bignum&);  //p =pmax{p1,p2} 
+		friend bignum operator-(const bignum&, const bignum&);  //p =pmax{p1,p2}
+		friend bignum operator*(const bignum&, const bignum&);  //p =pmax{p1,p2}
+		friend bignum operator*(const bignum&, const bignum&);  //p =pmax{p1,p2}
+
+		friend bignum const &operator=(const bignum &);
+
+		friend std::ostream& operator<<(std::ostream&, const bignum&);
+		friend std::istream& operator>>(std::istream&, bignum&);
+}
