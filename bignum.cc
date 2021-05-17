cae9164b02750eb04181446ec718a9f90d6abce2
lbuyi97 <lboudjikanian@fi.uba.ar>
2021-05-16 23:37:58 -0300

    primeros intentos de metodos

--- bignum.cc
+++ bignum.cc
@@ -1,7 +1,8 @@
 #include <iostream>
 #include "bignum.h"
+#define PRECISION_DEFAULT 10
 
-
+using namespace std;
 
 bignum operator+(const bignum& b1, const bignum& b2){
 	// A DESARROLLAR JUNTO AL RETURN
@@ -26,4 +27,36 @@ std::istream& operator>>(std::istream& is, bignum& b){
 	return is;
 }
 
+bignum::bignum() : negative(false)
+{		
+	return bignum(PRECISION_DEFAULT)	
+}
 
+bignum::bignum(unsigned short p) : precision(p) , negative(false)
+{	
+	digits = new unsigned short[precision]
+	for(int i; i<precision ; i++){
+		digits[i] = 0;
+	}
+	
+	return *this;
+	
+}
+
+complejo::complejo(double r) : re_(r), im_(0)
+{
+}
+
+complejo::complejo(double r, double i) : re_(r), im_(i)
+{
+}
+
+complejo::complejo(complejo const &c) : re_(c.re_), im_(c.im_)
+{
+}
+
+
+complejo::~complejo()
+{
+	delete []digits
+}
